0cb5a29519203a15470f97e6229efa743c3505ea
Sibgha Bhatti <106465420+SibghaBhatti@users.noreply.github.com>
2023-09-24 11:04:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function activeFormContainer() {
+  let form = document.getElementById("formC"); // Correct the typo in the ID here
+  // Toggle the "activeContainer" class
+  form.classList.toggle("activeContainer");
+}
